49db121b3cd48ead8ed46cc9d97170c638372dfb
ManuelDev <manueldeveloper17@gmail.com>
2023-01-11 09:04:10 -0500

    Add Menu component and Page Single post

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
-
-const Home = () => {
+const Menu = () => {
   const posts = [
     {
       id: 1,
@@ -33,28 +31,20 @@ const Home = () => {
   ];
 
   return (
-    <div className="home">
-      <div className="posts">
-        {
-          // Iterando sobre los posts
-          posts.map((post) => (
-            <div className="post" key={post.id}>
-              <div className="img">
-                <img src={post.image} alt={`Img ${post.title}`} />
-              </div>
-              <div className="content">
-                <Link className="link" to={`/post/${post.id}`}>
-                  <h1>{post.title}</h1>
-                </Link>
-                <p>{post.desc}</p>
-                <button>Read more</button>
-              </div>
-            </div>
-          ))
-        }
-      </div>
+    <div className="menu">
+      <h1>Other posts you may Like</h1>
+      {
+        // Iterando sobre los posts
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <img src={post.image} alt={`Img ${post.title}`} />
+            <h2>{post.title}</h2>
+            <button>Read More</button>
+          </div>
+        ))
+      }
     </div>
   );
 };
 
-export { Home };
+export { Menu };
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,5 +1,77 @@
+import { Link } from "react-router-dom";
+
+// Components
+import { Menu } from "../components/Menu";
+
+// Images
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/776656/pexels-photo-776656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Img post"
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Img user"
+          />
+          <div className="info">
+            <span>Manuel</span>
+            <p>Posted 2 days go</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="Icon edit" />
+            </Link>
+            <Link>
+              <img src={Delete} alt="Icon delete" />
+            </Link>
+          </div>
+        </div>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga libero,
+          accusamus saepe.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at
+          vulputate ipsum. Aenean tempus posuere erat. Quisque est leo, tempor
+          vitae varius accumsan, pretium vitae dolor. Nulla maximus magna massa,
+          vel cursus velit tristique blandit.
+          <br />
+          <br />
+          Nulla facilisi. Pellentesque rutrum sed sem lacinia bibendum. Donec
+          convallis purus et efficitur iaculis. Aenean mauris lorem, fermentum
+          ac varius sit amet, posuere id dui. Quisque pharetra ante neque, sit
+          amet mattis diam malesuada in. In tincidunt arcu eu eleifend placerat.
+          Nam pulvinar neque urna, nec placerat dolor tristique id.
+          <br />
+          <br />
+          Etiam interdum elit vel dolor porta, ac tincidunt tellus vehicula. Nam
+          lacinia accumsan nisl eget vulputate. Donec pretium felis at consequat
+          lobortis. Duis ac purus dictum, rhoncus ex id, fermentum nisl. Mauris
+          in libero turpis. Phasellus at bibendum risus. Sed posuere sit amet
+          risus eget laoreet. Sed mollis dignissim orci, sed ornare ipsum.
+          Integer vitae lobortis est.
+          <br />
+          <br />
+          Pellentesque rutrum sed sem lacinia bibendum. Donec convallis purus et
+          efficitur iaculis. Aenean mauris lorem, fermentum ac varius sit amet,
+          posuere id dui. Quisque pharetra ante neque, sit amet mattis diam
+          malesuada in. In tincidunt arcu eu eleifend placerat. Nam pulvinar
+          neque urna, nec placerat dolor tristique id. Etiam interdum elit vel
+          dolor porta, ac tincidunt tellus vehicula. Nam lacinia accumsan nisl
+          eget vulputate. Donec pretium felis at consequat lobortis. Duis ac
+          purus dictum, rhoncus ex id, fermentum nisl.
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export { Single };
